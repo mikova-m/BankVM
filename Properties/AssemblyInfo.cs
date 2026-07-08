ca2ce7d951837a00b89b973ccfd4a78c3f844400
Admin <Admin@MONIKA>
2026-07-08 16:20:04 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BankVM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankVM")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fbb56e55-0798-4b5c-9e07-be002423ba28")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReadWrite.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BankVM
+{
+    internal class ReadWrite
+    {
+        static string _filePath = @"..\..\Data\info.txt";
+
+        public static List<Person> ReadPeople()
+        {
+            List<Person> cars = new List<Person>();
+            string[] lines = File.ReadAllLines(_filePath); //ИЗИСКВА USING SYSTEM.IO;
+
+            for (int i = 1; i < lines.Length; i++) //Прескачаме антетката която е i=0
+            {
+                string[] parts = lines[i].Split(',');
+
+                string brand = parts[0];
+                string model = parts[1];
+                int year = int.Parse(parts[2]);
+                int price = int.Parse(parts[3]);
+
+                Person car = new Person(id, firstName, lastName, age, iban, amount, pass);
+                cars.Add(car);
+
+            }
+
+            return cars;
+        }
+
+        public static void UpdatePeopleFile()
+        {
+            string antetka = "ID, FirstName, LastName, Age, IBAN, Amount, Pass: ";
+            List<string> lines = new List<string>();
+
+            lines.Add(antetka);
+            foreach (Person c in Person.MyPeople) //обикаля
+            {
+                string info = c.ToString();//извлича като цсв текст
+                lines.Add(info);
+            }
+            File.WriteAllLines(_filePath, lines);//Същински запис във файла
+        }
+    }
+}
