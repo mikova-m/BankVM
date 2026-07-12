5549bfd211803eb4806f1b7813bf4275b6ebdcea
mikova-m <monikamikova032@gmail.com>
2026-07-12 16:39:48 +0300

    done class display(added the way to save the information)

--- Display.cs
+++ Display.cs
@@ -8,12 +8,13 @@ namespace BankVM
 {
     internal class Display
     {
-         public static void AccountMenu()
+         public static void AccountMenu(Person user)
          {
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("\n=== Моля изберете ===");
             Console.ResetColor();
 
+            Console.WriteLine($"Баланс: {user.Amount} евро");//добавено от нас, няма го като изискване, но е полезно и удобно
             Console.WriteLine("1) Добавяне на пари (депозит) ");
             Console.WriteLine("2) Изтегляне на пари");
             Console.WriteLine("3) Прехвърляне на пари към друг клиент");
@@ -31,14 +32,19 @@ namespace BankVM
 
             switch (choose)//Има грешка
             {
-                case 1: Person.AddMoney(); break;
-                case 2: Person.GetMoney(); break;
-                case 3: Person.SendMoney(); break;
-                case 4: Person.ChangePassword(); break;
+                case 1: Person.AddMoney(user); break;
+                case 2: Person.GetMoney(user); break;
+                case 3: Person.SendMoney(user); break;
+                case 4: Person.ChangePassword(user); break;
+                case 5: 
+                    ReadWrite.UpdatePeopleFile(); // записва промените във файла
+                    Console.WriteLine("Излизане от акаунта");
+                    Console.ReadLine();
+                    return;
                 default: Environment.Exit(0); break;
             }
 
-            AccountMenu();
+            AccountMenu(user);// ne sme sigurni dali trqbva
         }
     }
 }
